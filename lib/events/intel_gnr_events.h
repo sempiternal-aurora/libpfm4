4e6e05e469762c7a2772d5c37bd01a8e495866e0
Stephane Eranian <eranian@gmail.com>
2026-09-19 00:27:06 -0700

    Fix duplicated event code for IDQ_UOPS_NOT_DELIVERED on Intel GraniteRapid
    Event IDQ_UOPS_NOT_DELIVERED is replaced by IDQ_BUBBLES, so mark as
    deprecated to avoid duplicate event code error.
    
    Signed-off-by: Stephane Eranian <eranian@gmail.com>

--- lib/events/intel_gnr_events.h
+++ lib/events/intel_gnr_events.h
@@ -2609,7 +2609,7 @@ static const intel_x86_entry_t intel_gnr_pe[]={
     .modmsk = INTEL_V5_ATTRS,
     .cntmsk = 0xffull,
     .ngrp   = 1,
-    .flags  = INTEL_X86_SPEC,
+    .flags  = INTEL_X86_SPEC | INTEL_X86_DEPRECATED,
     .numasks= LIBPFM_ARRAY_SIZE(intel_gnr_idq_uops_not_delivered),
     .umasks = intel_gnr_idq_uops_not_delivered,
   },
